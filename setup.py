50780c60e52d0ead6a17e39275f60388cce56c06
Sakshi Gautam <gautamsakshi551@gmail.com>
2025-08-08 18:28:30 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="calculator",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Your Name",
+    description="A simple calculator package.",
+)
