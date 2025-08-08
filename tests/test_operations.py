ed15575a8dbd1a9c197ff577d6651445c4393e61
Sakshi Gautam <gautamsakshi551@gmail.com>
2025-08-08 18:58:58 +0530

    Update test_operations.py

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,17 +1,22 @@
 import pytest
 from calculator import operations
 
+
 def test_add():
     assert operations.add(2, 3) == 5
+
 
 def test_subtract():
     assert operations.subtract(5, 2) == 3
 
+
 def test_multiply():
     assert operations.multiply(3, 4) == 12
 
+
 def test_divide():
     assert operations.divide(10, 2) == 5
+
 
 def test_divide_by_zero():
     with pytest.raises(ValueError):
